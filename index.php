4095948a80b990141f0cb8fc5082dbf72e25aa62
Dejan-84 <dekidjurdjev@gmail.com>
2022-11-14 19:40:57 +0100

    adding files

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Digital Studio</title>
+<meta name="description" content="Kompanija koja pruza usluge digitalnog marketinga i web-developmenta">
+<meta name="author" content="Smart Academy">
+<meta name="keywords" content="marketing,agencija,brendiranje,aplikacije,web-development">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" href="index.php">Digital Studio</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#home" class="page-scroll">Početna</a></li>
+        <li><a href="#about-section" class="page-scroll">O nama</a></li>
+        <li><a href="#services-section" class="page-scroll">Servisi</a></li>
+        <li><a href="#works-section" class="page-scroll">Portfolio</a></li>
+        <li><a href="#team-section" class="page-scroll">Naš tim</a></li>
+        <li><a href="#contact-section" class="page-scroll">Kontakt</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+
+<!-- Header -->
+<header class="text-center" name="home">
+  <div class="intro-text">
+    <h1>Mi smo<span class="color"> Digital Studio</span></h1>
+    <p>Kompanija za digitalni dizajn i web-development</p>
+    <div class="clearfix"></div>
+    <a href="#about-section" class="btn btn-default btn-lg page-scroll">Saznaj Više</a> </div>
+</header>
+<!-- About Section -->
+<div id="about-section">
+  <div class="container">
+    <div class="section-title">
+      <h2>O Nama</h2>
+      <hr>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-md-4 text-left">
+        <h4>Ko smo mi</h4>
+        <p>Mi smo kreativni tim sastavljen od iskusnih web developera, dizajnera i marketing stručnjaka punih ideja, volje i energije za stvaranjem originalnih i kreativnih rešenja savremenog web dizajna. Ono što vam mi nudimo je profesionalna izrada sajtova, kao i kreiranje efektivnih marketinških strategija.</p>
+      </div>
+      <div class="col-md-4 text-left">
+        <h4>Šta radimo</h4>
+        <p>U zavisnosti od vaših potreba, pružamo usluge<br> web-dizajna, izrade web aplikacija, kreiranje online prodavnica sa cms rešenjima, audio i video produkciju. Takođe, svojim klijentima pružamo i usluge marketinga, brendiranja i kreiranja sadržaja za vaše web stranice.</p>
+      </div>
+      <div class="col-md-4 text-left">
+        <h4>Zašto odabrati nas</h4>
+        <p>Pri izradi sajtova koristimo najnovije svetske tehnologije. Što se tiče marketinga, uvek pratimo najnovije trendove i koristimo najefikasnije strategije. Vreme je najvrednija stvar koju čovek može potrošiti. Svesni te činjenice, posebnu pažnju posvećujemo dogovorima, rokovima i samoj organizaciji posla.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Services Section -->
+<div id="services-section">
+  <div class="container">
+    <div class="section-title">
+      <h2>Naše Usluge</h2>
+      <hr>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-desktop"></i>
+        <h4>Web design</h4>
+        <p>Web dizajn je planiranje i izrada web stranica. Ovaj proces uključuje dizajn korisničkog okruženja, izgleda stranica, navigacije, izgleda, boje, fontova i slika. Sve ovo ima za cilj stvaranje web sajta koja zadovoljava ciljeve vlasnika i očekivanja klijenata.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-gears"></i>
+        <h4>Razvoj Aplikacija</h4>
+        <p>Korišćenjem isključivo najsavremenijih tehnologija i tendencija u web dizajnu, programiranju i grafičkom interfejsu podižemo nivo Vaše poslovnosti i pomažemo u izgradnji Vašeg poslovnog ugleda, što će znatno povećati vašu interakciju sa klijentima.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-pie-chart"></i>
+        <h4>Analitika</h4>
+        <p>Ako želite da pratite koliko posetilaca dođe na vaš sajt ili blog dnevno, nedeljno, mesečno ili godišnje postoji veliki broj alata za prikupljanje ovih podataka.Trenutno je Google Analytics jedan od najpopularnijih programa za praćenje posećenosti veb sajta.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-line-chart"></i>
+        <h4>Marketing</h4>
+        <p>Nije važno da li ste kompanija većeg formata, zanatska radnja ili pojedinac, društvene mreže mogu svi da iskoriste kao potencijal, i u skladu sa svojom politikom poslovanja ukažu potencijalnim klijentima na svoju profesionalnost i vrednost.</p>
+      </div>
+    </div>
+    <div class="space"></div>
+    <div class="row text-left">
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-shopping-cart"></i>
+        <h4>eCommerce Rešenja</h4>
+        <p>U našoj ponudi možete, izmeđuostalog naći usluge izrade prodavnice koja je u suštini obavezna ukoliko se bavite trgovinom i prodajom artikala putem interneta. Nudimo izradu web shopa u okviru izrade web sajta ili ugradnju web prodavnice u postojeći sajt.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-file-text-o"></i>
+        <h4>Copywriting</h4>
+        <p>Copywriting je umetnost pisanja i izražavanja, sažimanja suštine vašeg posla u privlačne i kratke poruke upućene klijentu, na osnovu kojih će vas zapamtiti i kojima ćete ostaviti utisak profesionalnosti. Dobar copy će vam pomoći da osvojite poverenje klijenta. </p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-rocket"></i>
+        <h4>Brendiranje</h4>
+        <p>S marketinške tačke gledišta, stvaranje brenda tj. brendiranje firme, proizvoda, usluge ili ličnosti je osnov svakog uspeha. Uspeh u poslovanju nikada nije slučajan već je rezultat truda, rada, vremena, kreativnosti i drugih faktora koji su planski ulagani u njega.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 service"> <i class="fa fa-camera"></i>
+        <h4>Video & Audio produkcija</h4>
+        <p>Ukoliko vam je potrebna video ili audio produkcija na pravom ste mestu. Zajedno možemo osmisliti koncept reklame za radio, televiziju i youtube. Nakon toga vršimo snimanje materijala i sve što je vezano za video i audio produkciju.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="works-section">
+  <div class="container"> <!-- Container -->
+    <div class="section-title">
+      <h2>Naš Portfolio</h2>
+      <hr>
+      <div class="clearfix"></div>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">Sve</a></li>
+            <li><a href="#" data-filter=".web">Web Design</a></li>
+            <li><a href="#" data-filter=".app">Razvoj Aplikacija</a></li>
+            <li><a href="#" data-filter=".branding">Brendiranje</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 branding">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/01.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Brendiranje</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/01.jpg" class="img-responsive" alt="Razvoj Bliss brenda"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/02.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Razvoj Aplikacije</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/02.jpg" class="img-responsive" alt="Aplikacija za knjigovodstvo"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/03.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Web Design</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/03.jpg" class="img-responsive" alt="Dizajn web sajta"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/04.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Web Design</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/04.jpg" class="img-responsive" alt="Kreiranje web sajta kompanije Helios"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/05.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Razvoj Aplikacije</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/05.jpg" class="img-responsive" alt="Android aplikacija"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/06.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Web Design</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/06.jpg" class="img-responsive" alt="Web sajt agencije Modus"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/07.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Razvoj Aplikacije, Brendiranje</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/07.jpg" class="img-responsive" alt="Aplikacija za vremensku prognozu"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/08.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Vrsta Projekta</h4>
+                <small>Web Design</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img/portfolio/08.jpg" class="img-responsive" alt="Kreiranje web sajta za restoran"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Team Section -->
+<div id="team-section">
+  <div class="container">
+    <div class="section-title">
+      <h2>Upoznajte Naš Tim</h2>
+      <hr>
+    </div>
+    <div id="row">
+      <div class="col-md-3 col-sm-6 team">
+        <div class="thumbnail"> <img src="img/team/01.jpg" alt="..." class="team-img">
+          <div class="caption">
+            <h3>Marko Lukić</h3>
+            <p>CEO &amp; Founder</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 team">
+        <div class="thumbnail"> <img src="img/team/02.jpg" alt="..." class="team-img">
+          <div class="caption">
+            <h3>Nikola Pavlović</h3>
+            <p>Web Designer</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 team">
+        <div class="thumbnail"> <img src="img/team/03.jpg" alt="..." class="team-img">
+          <div class="caption">
+            <h3>Jasmina Ranković</h3>
+            <p>Creative Director</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 team">
+        <div class="thumbnail"> <img src="img/team/04.jpg" alt="..." class="team-img">
+          <div class="caption">
+            <h3>Dušan Jovanovski</h3>
+            <p>Web Developer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact-section">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Kontaktirajte Nas</h2>
+      <hr>
+    </div>
+    <div class="col-md-4">
+      <h4>Kontakt informacije</h4>
+      <div class="space"></div>
+      <p><i class="fa fa-map-marker"></i>Pop Lukina 23, Beograd-Zvezdara</p>
+      <div class="space"></div>
+      <p><i class="fa fa-envelope-o"></i>info@digitalstudio.com</p>
+      <div class="space"></div>
+      <p><i class="fa fa-phone"></i>+381 69 456 1234</p>
+    </div>
+    <div class="col-md-8">
+      <h4>Pošaljite nam poruku</h4>
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Ime i prezime" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Vaša poruka" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-default">Pošalji poruku</button>
+      </form>
+    </div>
+  </div>
+</div>
+<div id="social-section">
+  <div class="container">
+    <div class="social">
+      <ul>
+        <li><a href="https://www.facebook.com/SmartAkademija/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="https://twitter.com/SmartAkademija" target="_blank"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="https://www.instagram.com/smartakademija/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a href="#"><i class="fa fa-github"></i></a></li>
+
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <p>Copyright &copy; <a href="http://smartacademy.edu.rs/" rel="nofollow" target="_blank">Dejan Djurdjev 2021</a></p>
+  </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+
+<!-- Javascripts
+    ================================================== --> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
